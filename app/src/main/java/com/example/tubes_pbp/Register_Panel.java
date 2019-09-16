f82e8fdcac005f648f0cb71c937e31a7f882000b
Kampang666 <Kampang666@users.noreply.github.com>
2019-09-16 19:50:08 +0700

    v3

--- app/src/main/java/com/example/tubes_pbp/Register_Panel.java
+++ app/src/main/java/com/example/tubes_pbp/Register_Panel.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Register_Panel extends Login_Panel{
 
+    private TextView linkLogin;
     private Button btnDaftar;
 
     @Override
@@ -14,11 +16,20 @@ public class Register_Panel extends Login_Panel{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.panel_register);
 
+        linkLogin = (TextView) findViewById(R.id.linkLogin);
+        linkLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent i= new Intent(Register_Panel.this, Login_Panel.class);
+                startActivity(i);
+            }
+        });
+
         btnDaftar = (Button)findViewById(R.id.btnDaftar);
         btnDaftar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Register_Panel.this, Login_Panel.class);
+                Intent i = new Intent(Register_Panel.this, Home_Panel.class);
                 startActivity(i);
             }
         });
