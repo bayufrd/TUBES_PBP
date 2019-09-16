ce7485c17102ea762a295becec2de8e3d0fba799
bayufrd <55101246+bayufrd@users.noreply.github.com>
2019-09-16 22:27:01 +0700

    fungsi

--- app/src/main/java/com/example/tubes_pbp/Login_Panel.java
+++ app/src/main/java/com/example/tubes_pbp/Login_Panel.java
@@ -1,12 +1,38 @@
 package com.example.tubes_pbp;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.tubes_pbp.apihelper.BaseApiService;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Login_Panel extends Home_Panel{
+
+    EditText etEmail;
+    EditText etPassword;
+    Button btnLogin;
+    Button btnRegister;
+    ProgressDialog loading;
+
+    Context mContext;
+    BaseApiService mApiService;
 
     private Button btnLogin;
     private TextView linkRegister;
@@ -16,14 +42,9 @@ public class Login_Panel extends Home_Panel{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.panel_login);
 
-        btnLogin = (Button)findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(Login_Panel.this, HomeAfter_Panel.class);
-                startActivity(i);
-            }
-        });
+        mContext = this;
+        mApiService = UtilsApi.getAPIService(); // meng-init yang ada di package apihelper
+        initComponents();
 
         linkRegister = (TextView) findViewById(R.id.linkRegister);
         linkRegister.setOnClickListener(new View.OnClickListener() {
@@ -31,6 +52,70 @@ public class Login_Panel extends Home_Panel{
             public void onClick(View view) {
                 Intent i = new Intent(Login_Panel.this, Register_Panel.class);
                 startActivity(i);
+            }
+        });
+    }
+
+    private void initComponents() {
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loading = ProgressDialog.show(mContext, null, "Harap Tunggu...", true, false);
+                requestLogin();
+            }
+
+            private void requestLogin() {
+                private void requestLogin(){
+                    mApiService.loginRequest(etEmail.getText().toString(), etPassword.getText().toString())
+                            .enqueue(new Callback<ResponseBody>() {
+                                @Override
+                                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                                    if (response.isSuccessful()){
+                                        loading.dismiss();
+                                        try {
+                                            JSONObject jsonRESULTS = new JSONObject(response.body().string());
+                                            if (jsonRESULTS.getString("error").equals("false")){
+                                                // Jika login berhasil maka data nama yang ada di response API
+                                                // akan diparsing ke activity selanjutnya.
+                                                Toast.makeText(mContext, "BERHASIL LOGIN", Toast.LENGTH_SHORT).show();
+                                                String nama = jsonRESULTS.getJSONObject("user").getString("nama");
+                                                Intent intent = new Intent(mContext, HomeAfter_Panel.class);
+                                                intent.putExtra("result_nama", nama);
+                                                startActivity(intent);
+                                            } else {
+                                                // Jika login gagal
+                                                String error_message = jsonRESULTS.getString("error_msg");
+                                                Toast.makeText(mContext, error_message, Toast.LENGTH_SHORT).show();
+                                            }
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                    } else {
+                                        loading.dismiss();
+                                    }
+                                }
+
+                                @Override
+                                public void onFailure(Call<ResponseBody> call, Throwable t) {
+                                    Log.e("debug", "onFailure: ERROR > " + t.toString());
+                                    loading.dismiss();
+                                }
+                            });
+                }
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(mContext, Register_Panel.class));
             }
         });
     }
--- app/src/main/java/com/example/tubes_pbp/Register_Panel.java
+++ app/src/main/java/com/example/tubes_pbp/Register_Panel.java
@@ -1,20 +1,52 @@
 package com.example.tubes_pbp;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.tubes_pbp.apihelper.BaseApiService;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Register_Panel extends Login_Panel{
 
+    EditText etNama;
+    EditText etEmail;
+    EditText etPassword;
+    Button btnRegister;
+    ProgressDialog loading;
+
+    Context mContext;
+    BaseApiService mApiService;
+
     private TextView linkLogin;
     private Button btnDaftar;
+    private UtilsApi UtilsApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.panel_register);
+
+        mContext = this;
+        mApiService = UtilsApi.getAPIService();
+
+        initComponents();
 
         linkLogin = (TextView) findViewById(R.id.linkLogin);
         linkLogin.setOnClickListener(new View.OnClickListener(){
@@ -31,6 +63,61 @@ public class Register_Panel extends Login_Panel{
             public void onClick(View view) {
                 Intent i = new Intent(Register_Panel.this, Login_Panel.class);
                 startActivity(i);
+            }
+        });
+    }
+
+    public void initComponents{
+        etNama = (EditText) findViewById(R.id.etNama);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loading = ProgressDialog.show(mContext, null, "Harap Tunggu...", true, false);
+                requestRegister();
+            }
+
+            private void requestRegister() {
+                private void requestRegister(){
+                    mApiService.registerRequest(etNama.getText().toString(),
+                            etEmail.getText().toString(),
+                            etPassword.getText().toString())
+                            .enqueue(new Callback<ResponseBody>() {
+                                @Override
+                                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                                    if (response.isSuccessful()){
+                                        Log.i("debug", "onResponse: BERHASIL");
+                                        loading.dismiss();
+                                        try {
+                                            JSONObject jsonRESULTS = new JSONObject(response.body().string());
+                                            if (jsonRESULTS.getString("error").equals("false")){
+                                                Toast.makeText(mContext, "BERHASIL REGISTRASI", Toast.LENGTH_SHORT).show();
+                                                startActivity(new Intent(mContext, Login_Panel.class));
+                                            } else {
+                                                String error_message = jsonRESULTS.getString("error_msg");
+                                                Toast.makeText(mContext, error_message, Toast.LENGTH_SHORT).show();
+                                            }
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                    } else {
+                                        Log.i("debug", "onResponse: GA BERHASIL");
+                                        loading.dismiss();
+                                    }
+                                }
+
+                                @Override
+                                public void onFailure(Call<ResponseBody> call, Throwable t) {
+                                    Log.e("debug", "onFailure: ERROR > " + t.getMessage());
+                                    Toast.makeText(mContext, "Koneksi Internet Bermasalah", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }
             }
         });
     }
