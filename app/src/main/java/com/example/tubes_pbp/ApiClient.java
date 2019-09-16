b9fe35bdc847a5ec5d7379e1b6e78a5ec6a84b82
bayufrd <55101246+bayufrd@users.noreply.github.com>
2019-09-16 19:50:33 +0700

    aa

--- app/src/main/java/com/example/tubes_pbp/ApiClient.java
+++ app/src/main/java/com/example/tubes_pbp/ApiClient.java
@@ -1,5 +1,10 @@
 package com.example.tubes_pbp;
 
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
 public class ApiClient {
 
     public static final String BASE_URL ="#belum dibuat";
--- app/src/main/java/com/example/tubes_pbp/ApiInterface.java
+++ app/src/main/java/com/example/tubes_pbp/ApiInterface.java
@@ -2,6 +2,14 @@ package com.example.tubes_pbp;
 
 import java.util.List;
 
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
 public interface ApiInterface {
     @GET("akun/tampil")
     Call<List<RegisterDAO>> getRegister();
